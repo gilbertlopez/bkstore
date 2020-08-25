fe461d6346dec747c0cb8d3a5d499e565260823f
gillo <gillo@DESKTOP-6GTM6BB>
2020-08-25 13:26:22 -0700

    Init commit

---
+++ bkstore/src/test/java/com/gillo/bookstore/BkstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gillo.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
