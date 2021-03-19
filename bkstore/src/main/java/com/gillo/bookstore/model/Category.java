8e47f3dad8a860c31ec78e1b5afd7c8e5c7e0d85
Gilbert Lopez <mrgillopez@att.net>
2021-03-19 12:52:27 -0700

    Changed PK generation strategy to IDENTITY to fix problem data.sql issue

--- bkstore/src/main/java/com/gillo/bookstore/model/Category.java
+++ bkstore/src/main/java/com/gillo/bookstore/model/Category.java
@@ -25,7 +25,7 @@ import lombok.Data;
 public class Category {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@NotNull
---
+++ bkstore/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO category (name) VALUES ('Fantasy');
+INSERT INTO category (name) VALUES ('Programming');
+INSERT INTO category (name) VALUES ('Business');
+INSERT INTO category (name) VALUES ('Romance');
+INSERT INTO category (name) VALUES ('Poetry');
+INSERT INTO category (name) VALUES ('Cookbooks, Food & Wine');
+INSERT INTO category (name) VALUES ('History');
